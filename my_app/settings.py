8e22adeccacb69016da486aa2b8816f08fe90cbc
frankjam <frankjamlick@gmail.com>
2024-04-26 23:33:25 +0300

    domains allowed to access platform

--- my_app/settings.py
+++ my_app/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-9rnkj@5-(!_njf6m3f(0deltp5@3w@swm@9rf3$@otq$5(!v6t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "*",
+]
 
 
 # Application definition
